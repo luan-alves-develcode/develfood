b8135e6fb312344ed902a6762ee5fe6d17c17486
Luan Alves <luan.alves@develcode.com.br>
2024-03-11 14:55:33 -0300

    initial commit

---
+++ src/main/java/com/develcode/develfood/DevelfoodApplication.java
@@ -0,0 +1,13 @@
+package com.develcode.develfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/develcode/develfood/DevelfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develcode.develfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
