535dd011f256e82bfa2f03b2a3dc34b95295d94c
Luan Alves <luan.alves@develcode.com.br>
2024-03-14 14:14:19 -0300

    feat: create FoodType entity

--- src/main/java/com/develcode/develfood/model/FoodType.java
+++ src/main/java/com/develcode/develfood/model/FoodType.java
@@ -1,5 +1,19 @@
 package com.develcode.develfood.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
 public class FoodType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
     private String name;
 }
