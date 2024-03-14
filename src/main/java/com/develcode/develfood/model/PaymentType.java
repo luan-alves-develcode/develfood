3ede561ab170eca20dec9790b7957ee7014c8b7b
Luan Alves <luan.alves@develcode.com.br>
2024-03-14 15:14:08 -0300

    rename attribute

--- src/main/java/com/develcode/develfood/model/PaymentType.java
+++ src/main/java/com/develcode/develfood/model/PaymentType.java
@@ -3,9 +3,9 @@ package com.develcode.develfood.model;
 public enum PaymentType {
     CARTAODEBITO("Cartão de Débito"), CARTAOCREDITO("Cartão de Cŕedito"), PIX("Pix"), DINHEIRO("Dinheiro");
 
-    private final String nome;
+    private final String name;
 
-    PaymentType(String nome) {
-        this.nome = nome;
+    PaymentType(String name) {
+        this.name = name;
     }
 }
