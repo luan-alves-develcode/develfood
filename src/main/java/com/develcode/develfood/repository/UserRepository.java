96da43d88d263b94975f7cbdab65aac8fdbdde2f
Luan Alves <luan.alves@develcode.com.br>
2024-03-14 15:59:46 -0300

    feat: create User repo, Authentication service and Error handler

---
+++ src/main/java/com/develcode/develfood/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.develcode.develfood.repository;
+
+import com.develcode.develfood.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByLogin(String login);
+}
---
+++ src/main/java/infra/exception/ValidationErrorData.java
@@ -0,0 +1,13 @@
+package infra.exception;
+
+import org.springframework.validation.FieldError;
+
+public class ValidationErrorData {
+    private String field;
+    private String message;
+
+    public ValidationErrorData(FieldError fieldError) {
+        this.field = fieldError.getField();
+        this.message = fieldError.getDefaultMessage();
+    }
+}
