711f233e02c17d7468974359eca0006d32f828a4
Luan Alves <luan.alves@develcode.com.br>
2024-03-14 14:51:28 -0300

    feat: create RestaurantEvaluation entity and optimize imports

--- src/main/java/com/develcode/develfood/model/RestaurantEvaluation.java
+++ src/main/java/com/develcode/develfood/model/RestaurantEvaluation.java
@@ -9,31 +9,30 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.Setter;
 
-import java.math.BigDecimal;
+import java.util.Date;
 
 @Entity
 @Getter
-@Table(name = "plates")
-public class Plate {
+@Table(name = "restaurant_evaluations")
+public class RestaurantEvaluation {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
 
-    private String name;
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
 
-    private String description;
-
-    private BigDecimal price;
-
+    @Setter
     @ManyToOne
     @JoinColumn(name = "restaurant_id")
     private Restaurant restaurant;
 
-    @ManyToOne
-    @JoinColumn(name = "food_type_id")
-    private FoodType foodType;
+    private Integer grade;
 
-
+    private Date evaluationDate;
 }
