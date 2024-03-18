52719cdea6220c75e87227ebd7357345689764a9
Luan Alves <barkon32@tuta.io>
2024-03-18 16:56:49 -0300

    feat: created dto for user sign up

---
+++ src/main/java/com/develcode/develfood/dto/UserSignUpDto.java
@@ -0,0 +1,27 @@
+package com.develcode.develfood.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Length;
+
+public class UserSignUpDto {
+
+    @Length(max = 50)
+    private String firstName;
+    @Length(max = 150)
+    private String lastName;
+
+    @Pattern(regexp = "[0-9]{3}[\\.][0-9]{3}[\\.][0-9]{3}[\\-][0-9]{2}")
+    private String cpf;
+
+    @Length(max = 18)
+    private String phone;
+
+    @Email
+    @Length(max = 100)
+    private String email;
+
+    @Size(min = 8, max = 80)
+    private String password;
+}
