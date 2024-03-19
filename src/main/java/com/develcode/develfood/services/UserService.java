167fffa30e84c0daa47bab9b1239cc02de46315b
Luan Alves <luan.alves@develcode.com.br>
2024-03-19 16:23:53 -0300

    feat: create function to handle user sign up

--- src/main/java/com/develcode/develfood/services/UserService.java
+++ src/main/java/com/develcode/develfood/services/UserService.java
@@ -1,35 +1,39 @@
 package com.develcode.develfood.services;
 
 import com.develcode.develfood.dto.CustomerSignUpDto;
+import com.develcode.develfood.dto.CustomerDataDto;
 import com.develcode.develfood.model.Customer;
 import com.develcode.develfood.model.User;
 import com.develcode.develfood.repository.CustomerRepository;
 import com.develcode.develfood.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.develcode.develfood.services.exception.EmailExistsException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserService {
 
-    @Autowired
     PasswordEncoder passwordEncoder;
 
-    @Autowired
     UserRepository userRepository;
 
-    @Autowired
     CustomerRepository customerRepository;
 
-    public User signUpNewUser(CustomerSignUpDto userDto) throws EmailExistsException{
-        if (emailExist(userDto.getEmail())) {
+    public CustomerDataDto signUpNewUser(CustomerSignUpDto userDto) throws EmailExistsException {
+        if (userRepository.existsByEmail(userDto.getEmail())) {
             throw new EmailExistsException(
-                    "There is an account with that email adress:" + userDto.getEmail());
+                    "Email address already in use: " + userDto.getEmail());
         }
         var user = new User(userDto.getEmail(), passwordEncoder.encode(userDto.getPassword()));
 
-        userRepository.save(user);
+        var savedUser = userRepository.save(user);
 
-        var customer = new Customer(userDto.getFirstName(), userDto.getLastName(), userDto.getCpf(), userDto.getPhone());
+        var customer = new Customer(userDto.getFirstName(), userDto.getLastName(), userDto.getCpf(), userDto.getPhone(), savedUser);
 
-        customerRepository.save(customer);
+        var savedCustomer = customerRepository.save(customer);
+
+        return new CustomerDataDto(savedUser, savedCustomer);
     }
 }
