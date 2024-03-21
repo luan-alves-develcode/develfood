2acc08e1b56268acc8a7088cba591c940e539fbf
Luan Alves <luan.alves@develcode.com.br>
2024-03-21 14:56:12 -0300

    feat: create Restaurant repository interface

---
+++ src/main/java/com/develcode/develfood/repository/RestaurantRepository.java
@@ -0,0 +1,7 @@
+package com.develcode.develfood.repository;
+
+import com.develcode.develfood.model.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+}
