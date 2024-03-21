1f527a959ef2fec60c39ea0a4a2aeddfcbde1885
Luan Alves <luan.alves@develcode.com.br>
2024-03-21 17:31:41 -0300

    feat: create custom exceptions

---
+++ src/main/java/com/develcode/develfood/services/exception/CnpjExistsException.java
@@ -0,0 +1,5 @@
+package com.develcode.develfood.services.exception;
+
+public class CnpjExistsException extends RuntimeException{
+    public CnpjExistsException(String message) {super(message);}
+}
---
+++ src/main/java/com/develcode/develfood/services/exception/CpfExistsException.java
@@ -0,0 +1,5 @@
+package com.develcode.develfood.services.exception;
+
+public class CpfExistsException extends RuntimeException{
+    public CpfExistsException(String message) {super(message);}
+}
