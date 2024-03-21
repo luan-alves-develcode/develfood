72fe53f2b7cb882f800f55388f84a2fbb4a246ac
Luan Alves <luan.alves@develcode.com.br>
2024-03-21 14:56:53 -0300

    feat: create Restaurant controller and sign up action

---
+++ src/main/java/com/develcode/develfood/controller/RestaurantController.java
@@ -0,0 +1,30 @@
+package com.develcode.develfood.controller;
+
+
+import com.develcode.develfood.dto.CustomerDataDto;
+import com.develcode.develfood.dto.CustomerSignUpDto;
+import com.develcode.develfood.dto.RestaurantDataDto;
+import com.develcode.develfood.dto.RestaurantSignUpDto;
+import com.develcode.develfood.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/restaurante")
+public class RestaurantController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/cadastro")
+    public ResponseEntity<RestaurantDataDto> signUp(@RequestBody @Valid RestaurantSignUpDto restaurantSignUpDto) {
+        var newRestaurant = userService.newRestaurantSignUp(restaurantSignUpDto);
+
+        return ResponseEntity.ok(newRestaurant);
+    }
+}
