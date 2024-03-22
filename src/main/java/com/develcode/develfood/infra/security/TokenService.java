8ca994eb962ca58057b31473d063b8de856ef067
Luan Alves <luan.alves@develcode.com.br>
2024-03-22 13:34:41 -0300

    fix: remove print of secret from code

--- src/main/java/com/develcode/develfood/infra/security/TokenService.java
+++ src/main/java/com/develcode/develfood/infra/security/TokenService.java
@@ -18,7 +18,7 @@ public class TokenService {
     private String secret;
 
     public String generateToken(User user) {
-        System.out.println(secret);
+        
         try {
             var algorithm = Algorithm.HMAC256(secret);
             return JWT.create().withIssuer("develfoood")
