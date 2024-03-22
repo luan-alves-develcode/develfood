ee6bf242fd34df8662337449a10f76978d3fe38f
Luan Alves <luan.alves@develcode.com.br>
2024-03-22 14:39:37 -0300

    feat: add security filter to application

---
+++ src/main/java/com/develcode/develfood/infra/security/SecurityFilter.java
@@ -0,0 +1,30 @@
+package com.develcode.develfood.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var JwtToken = retrieveToken(request);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String retrieveToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader == null) {
+            throw new RuntimeException("Token not present in header");
+        }
+
+        return authorizationHeader;
+    }
+}
