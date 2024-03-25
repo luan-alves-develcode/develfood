7ae7011dd473fea5d7f44ab471616686c825753e
Luan Alves <luan.alves@develcode.com.br>
2024-03-25 11:21:54 -0300

    implement RequiredArgsConstructor on class

--- src/main/java/com/develcode/develfood/controller/CustomerController.java
+++ src/main/java/com/develcode/develfood/controller/CustomerController.java
@@ -6,7 +6,6 @@ import com.develcode.develfood.dto.CustomerSignUpDto;
 import com.develcode.develfood.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cliente")
+@RequiredArgsConstructor
 public class CustomerController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
     @PostMapping("/cadastro")
     public ResponseEntity<CustomerDataDto> signUp(@RequestBody @Valid CustomerSignUpDto customerSignUpDto) {
