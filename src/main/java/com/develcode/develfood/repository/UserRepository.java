0ca60e0fc7a205eb431b2eea90de0c08480fa035
Luan Alves <luan.alves@develcode.com.br>
2024-03-27 11:36:29 -0300

    feat: return optional from query instead of entity

--- src/main/java/com/develcode/develfood/repository/UserRepository.java
+++ src/main/java/com/develcode/develfood/repository/UserRepository.java
@@ -2,10 +2,11 @@ package com.develcode.develfood.repository;
 
 import com.develcode.develfood.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    UserDetails findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
 }
