730a90c4da93fb2d73e451f81243b9e9782dec5e
Luan Alves <luan.alves@develcode.com.br>
2024-03-27 11:34:52 -0300

    feat: add constructor

--- src/main/java/com/develcode/develfood/model/Address.java
+++ src/main/java/com/develcode/develfood/model/Address.java
@@ -5,8 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -31,4 +29,12 @@ public class Address {
     private String city;
 
     private String zipCode;
+
+    public Address(String street, Integer number, String neighbourhood, String city, String zipCode) {
+        this.street = street;
+        this.number = number;
+        this.neighbourhood = neighbourhood;
+        this.city = city;
+        this.zipCode = zipCode;
+    }
 }
--- src/main/resources/db/migration/V14__alter-table-restaurants-add-col.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE restaurants ADD user_id BIGINT NOT NULL;
-ALTER TABLE restaurants ADD CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES users (id);
