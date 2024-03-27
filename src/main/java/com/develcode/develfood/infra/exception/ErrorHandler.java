8118739581ace906155ef155c80c2c8a9bb431e9
Luan Alves <luan.alves@develcode.com.br>
2024-03-27 15:28:23 -0300

    feat: added type to ResponseEntity in error handling

--- src/main/java/com/develcode/develfood/infra/exception/ErrorHandler.java
+++ src/main/java/com/develcode/develfood/infra/exception/ErrorHandler.java
@@ -9,30 +9,32 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+
 @RestControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity handleErrorNotFound() {
+    public ResponseEntity<String> handleErrorNotFound() {
         return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity handleErrorArgumentNotValid(MethodArgumentNotValidException exception) {
+    public ResponseEntity<List<ValidationErrorData>> handleErrorArgumentNotValid(MethodArgumentNotValidException exception) {
         var errors = exception.getFieldErrors();
 
         return ResponseEntity.badRequest().body(errors.stream().map(ValidationErrorData::new).toList());
     }
 
     @ExceptionHandler(ValidationException.class)
-    public ResponseEntity handleErrorValidationException(ValidationException exception) {
+    public ResponseEntity<String> handleErrorValidationException(ValidationException exception) {
         var errors = exception.getMessage();
 
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errors);
     }
 
     @ExceptionHandler(JWTVerificationException.class)
-    public ResponseEntity handleJWTVerificationException(JWTVerificationException exception) {
+    public ResponseEntity<String> handleJWTVerificationException(JWTVerificationException exception) {
         var errors = exception.getMessage();
 
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errors);
